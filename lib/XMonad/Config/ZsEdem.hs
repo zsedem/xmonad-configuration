480573d3346148c985463914b27a4cf9b8ef765c
Zsigmond Adam Oliver <adam.zsigmond@balabit.com>
2017-04-29 10:53:37 +0200

    initial commit, this is copy pasted from my dotfiles repo
    Signed-off-by: Zsigmond Adam Oliver <adam.zsigmond@balabit.com>

---
+++ lib/XMonad/Config/ZsEdem.hs
@@ -0,0 +1,8 @@
+module XMonad.Config.ZsEdem(
+    module All
+) where
+
+import XMonad.Config.ZsEdem.Core as All
+import XMonad.Config.ZsEdem.Keys as All
+import XMonad.Config.ZsEdem.UrgencyHook as All
+import XMonad.Config.ZsEdem.Startup as All

447ae3fddec59dfce00aab27e500bc44ef1dab27
Zsigmond Adam Oliver <adam.zsigmond@balabit.com>
2017-07-07 15:39:41 +0200

    turn on automatic screen locking
    Signed-off-by: Zsigmond Adam Oliver <adam.zsigmond@balabit.com>

--- lib/XMonad/Config/ZsEdem/Startup.hs
+++ lib/XMonad/Config/ZsEdem/Startup.hs
@@ -10,6 +10,8 @@ startupHook = do
           [ "xsetroot -cursor_name left_ptr"
           , "nitrogen --restore"
           , "dunst -conf ~/.dunstrc"
+          , "xss-lock -n 'notify-send \"locking screen\"' hlock"
+          , "xset s 60 5"
           , "nm-applet"]
   where
         execOnce = spawn . ("exec "++)
